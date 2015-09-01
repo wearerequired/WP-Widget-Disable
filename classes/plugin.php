56c2e58b7dc5563ddf8671dda299d6c1c1bfb129
Pascal Birchler <hello@pascalbirchler.ch>
2015-09-01 16:42:48 +0200

    Version bump

--- classes/plugin.php
+++ classes/plugin.php
@@ -11,7 +11,7 @@ class WP_Widget_Disable_Plugin extends WP_Stack_Plugin2 {
 	/**
 	 * Plugin version.
 	 */
-	const VERSION = '1.2.0';
+	const VERSION = '1.3.0';
 
 	/**
 	 * Sidebar Widgets option key.
