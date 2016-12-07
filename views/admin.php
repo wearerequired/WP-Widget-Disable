14e6bed4e35a01bc62742ee79e3cab97e39c9650
Dominik Schilling <dominikschilling+git@gmail.com>
2016-12-07 23:53:33 +0100

    Change settings heading to h1
    See https://make.wordpress.org/core/2015/10/28/headings-hierarchy-changes-in-the-admin-screens/

--- views/admin.php
+++ views/admin.php
@@ -14,7 +14,7 @@
 ?>
 
 <div class="wrap">
-	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
 	<?php
 	$active_tab = $this->sidebar_widgets_option;
