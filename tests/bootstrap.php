2a73bf2d46998f6374d2b95a02f76c2c3c3a3876
Ulrich Pogson <grapplerulrich@gmail.com>
2022-03-16 12:24:47 +0100

    Require yoast/phpunit-polyfills

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,8 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+require dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
 // Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
 $_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
 if ( false !== $_phpunit_polyfills_path ) {
